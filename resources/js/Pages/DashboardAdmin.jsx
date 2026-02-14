2b7c1ab4eec8c695d167b5be0169c399751f6320
MTJM <jaudelmerlando@gmail.com>
2026-02-14 03:21:45 +0100

    composant DashboadAmin added

---
+++ resources/js/Pages/DashboardAdmin.jsx
@@ -0,0 +1,236 @@
+import SidebarEmploye from '@/Layouts/SidebarEmploye'
+import React from 'react'
+import { useState } from 'react'
+import { FaCoins, FaMoneyBill, FaMoneyBillAlt, FaMoneyBillWave, FaStore ,FaUser} from 'react-icons/fa'
+import { FiArrowUpRight, FiBox, FiCalendar, FiAlertTriangle, FiArrowDownRight, FiThumbsDown, FiDownloadCloud, FiUploadCloud, FiArchive, FiMenu, FiCheck, FiPlus } from 'react-icons/fi'
+
+
+const DashboardAdmin = () => {
+
+    const date = new Date()
+    const dateFormatee = date.toLocaleDateString('fr-FR', {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+        year: "numeric",
+    })
+
+    const commercants = [
+        { id: 1, nom: "Supermarché Central", created_at: "2024-01-15", abonnement: "actif" },
+        { id: 2, nom: "Boulangerie du Centre", created_at: "2024-02-10", abonnement: "essai" },
+        { id: 3, nom: "Librairie Saint-Michel", created_at: "2024-03-05", abonnement: "essai" },
+        { id: 4, nom: "Pharmacie de la Gare", created_at: "2024-04-20", abonnement: "actif" }
+    ]
+
+    const activitesRecentes = [
+        { id: 1, activite: "Nouvel abonnement", message: "Super Market Dupont a souscrit au plan Professionnel", date: "2025-01-15", icon: <FiCheck className="text-green-500" /> },
+        { id: 2, activite: "Modification d'abonnement", message: "Boulangerie du Centre a modifié son abonnement", date: "2025-02-10", icon: <FiArrowUpRight className="text-blue-500" /> },
+        { id: 3, activite: "Expiration d'abonnement", message: "Librairie Saint-Michel a expiré son abonnement", date: "2025-03-05", icon: <FiAlertTriangle className="text-yellow-500" /> },
+        { id: 4, activite: "Nouveau commerçant", message: "Pharmacie de la Gare a été ajoutée à la plateforme", date: "2025-04-15", icon: <FiPlus className="text-purple-500" /> },
+        { id: 5, activite: "Compte suspendu", message: "Compte de Super Market Dupont suspendu pour non-paiement", date: "2025-05-10", icon: <FiThumbsDown className="text-red-500" /> }
+    ]
+
+    const abonnements = [
+        { id: 1, commerce: "Supermarché Central", plan: 'professionnel', date_expiration: "2025-06-30", statut: "Actif" },
+        { id: 2, commerce: "Boulangerie du Centre", plan: 'premium', date_expiration: "2025-07-15", statut: "expiré" },
+        { id: 3, commerce: "Librairie Saint-Michel", plan: 'standard', date_expiration: "2025-05-20", statut: "expiré" },
+        { id: 4, commerce: "Pharmacie de la Gare", plan: 'premium', date_expiration: "2025-08-10", statut: "Actif" }
+    ]
+    const [sidebarOpen, setSidebarOpen] = useState(false)
+    return (
+        <div>
+            <div className='flex bg-secondary'>
+                {sidebarOpen && (
+                    <div
+                        className="fixed inset-0 bg-black/40 z-40 md:hidden"
+                        onClick={() => setSidebarOpen(false)}
+                    ></div>
+                )}
+                <SidebarEmploye sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen}></SidebarEmploye>
+                <div className='relative md:ml-64 bg-secondary max-w-max mb-20'>
+                    <div className='fixed flex items-center w-full gap-5 pl-5 md:pr-64 justify-between bg-white p-5'>
+                        <div className='flex justify-between items-center md:hidden shadow-md' onClick={() => setSidebarOpen(!sidebarOpen)}>
+                            <button className='bg-primary text-white px-4 py-4 rounded-lg'><FiMenu></FiMenu></button>
+                        </div>
+                        <div className='flex flex-col'>
+                            <h1 className='font-bold text-2xl'>Dashboard Administrateur</h1>
+                            <p className='text-text-medium'>Vue d'ensemble de la plateforme EasyStore</p>
+                        </div>
+                        <p className='flex gap-3 items-center border border-primary-dark bg-purple-200 text-primary px-5 md:py-2 rounded-lg mr-4'>
+                            <FiCalendar></FiCalendar>
+                            {dateFormatee.charAt(0).toUpperCase() + dateFormatee.slice(1)}
+                        </p>
+                    </div>
+
+                    {/* corps du dashboard */}
+                    <div className=''>
+                        {/* card */}
+                        <div className='flex md:flex-row flex-col md:justify-between gap-3 mt-28 px-5'>
+                            <div className='bg-white rounded-lg px-5 py-5 flex gap-4 items-center border'>
+
+                                <div className='flex flex-col gap-1'>
+                                    <div className='flex gap-3'>
+                                        <span className='text-text-medium'>Total commerçants</span>
+                                        <div className='w-14 h-14 bg-blue-200 flex justify-center items-center rounded-lg'><FaStore size={20} className='text-text-dark'></FaStore></div>
+                                    </div>
+
+                                    <p>
+                                        <span className='text-3xl font-bold block mb-3'>247</span>
+                                        <span className='flex items-center text-green-500'><FiArrowUpRight></FiArrowUpRight> +8 </span>
+                                        <span className='text-green-500'>ce mois</span>
+                                    </p>
+                                </div>
+                            </div>
+                            {/* valeur en stock */}
+
+                            <div className='bg-white rounded-lg px-5 py-5 flex gap-4 items-center border'>
+
+                                <div className='flex flex-col gap-1'>
+                                    <div className='flex gap-3'>
+                                        <span className='text-text-medium'>Abonnements Actifs</span>
+                                        <div className='w-14 h-14 bg-green-200 flex justify-center items-center rounded-lg'><FiCheck size={20} className='text-green-700'></FiCheck></div>
+                                    </div>
+
+                                    <p>
+                                        <span className='text-3xl font-bold'>218</span>
+                                        <span className='flex items-center text-green-500'><FiArrowUpRight></FiArrowUpRight> +8.5% </span>
+                                        <span className='text-green-500'>du taux d'activité</span>
+                                    </p>
+                                </div>
+                            </div>
+                            {/* Alertes actives */}
+
+                            <div className='bg-white rounded-lg px-5 py-5 flex gap-4 items-center border'>
+
+                                <div className='flex flex-col gap-1'>
+                                    <div className='flex gap-3'>
+                                        <span className='text-text-medium'>Abonnements Expirés</span>
+                                        <div className='w-14 h-14 bg-red-200 flex justify-center items-center rounded-lg'><FiAlertTriangle size={20} className='text-red-700'></FiAlertTriangle></div>
+                                    </div>
+
+                                    <p>
+                                        <span className='text-3xl font-bold'>29</span>
+                                        <span className='flex items-center text-red-500'><FiArrowUpRight></FiArrowUpRight> -5 </span>
+                                        <span className='text-red-500'>vs mois dernier</span>
+                                    </p>
+                                </div>
+                            </div>
+
+                            {/* Sorties Aujourd'hui */}
+
+                            <div className='bg-white rounded-lg px-5 py-5 flex gap-4 items-center border'>
+
+                                <div className='flex flex-col gap-1'>
+                                    <div className='flex gap-3'>
+                                        <span className='text-text-medium'>Revenus Mensuels</span>
+                                        <div className='w-14 h-14 bg-green-200 flex justify-center items-center rounded-lg'><FaCoins size={20} className='text-text-dark'></FaCoins></div>
+                                    </div>
+
+                                    <p>
+                                        <span className='text-3xl font-bold'>2.4M</span>
+                                        <span className='flex items-center text-green-500'><FiArrowUpRight></FiArrowUpRight> +8.5% </span>
+                                        <span className='text-green-500'>ce mois</span>
+                                    </p>
+                                </div>
+                            </div>
+
+
+                        </div>
+
+                        <div className='flex md:flex-row flex-col gap-4   md:p-5 p-2 '>
+
+                            <div className='rounded-lg border bg-white  md:p-5 p-2 md:w-2/3'>
+                                <h1 className='font-bold text-xl'>Activité récente</h1>
+                                <div className='mt-4'>
+                                    {activitesRecentes.map((activite) => (
+                                        <div key={activite.id} className='flex items-center gap-3 py-5 border-b'>
+                                            <div className='w-10 h-10 bg-blue-100 flex justify-center items-center rounded-lg'>
+                                                {activite.icon}
+                                            </div>
+                                            <div>
+                                                <p className='font-medium'>{activite.activite}</p>
+                                                <p className='text-sm text-text-medium'>{activite.message}</p>
+                                            </div>
+                                        </div>
+                                        
+                                    ))}
+                                </div>
+
+                            </div>
+                            <div className='border rounded-lg bg-white  md:p-5 p-2 md:w-1/3'>
+                                <h1 className='font-bold text-xl'>Nouveaux commerçants</h1>
+                                <div className='mt-4'>
+                                    {commercants.map((commerçant) => (
+                                        <div key={commerçant.id} className='flex items-center gap-3 py-5 border-b'>
+                                            <div className='w-10 h-10 bg-green-100 flex justify-center items-center rounded-lg'>
+                                                <FaUser size={20} className='text-green-700'></FaUser>
+                                            </div>
+                                            <div>
+                                                <p className='font-medium'>{commerçant.nom}</p>
+                                                <p className='text-sm text-text-medium'>inscrit le {commerçant.dateInscription}</p>
+                                            </div>
+                                            <span className={`px-2 py-1 rounded-lg text-xs font-medium ${commerçant.abonnement === 'actif' ? 'bg-green-100 text-green-800' : commerçant.abonnement === 'essai' ? 'bg-yellow-100 text-yellow-800' : 'bg-red-100 text-red-800'}`}>
+                                                {commerçant.abonnement}
+                                            </span>
+                                        </div>
+                                    ))}
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                        <div className='border bg-white rounded-lg md:m-5 md:p-5 p-2'>
+                            <div className='mb-5'>
+                                <h1 className='font-bold text-xl'>Abonnements nécessitant attention</h1>
+
+                            </div>
+                            <hr />
+                            <div className='w-full'>
+                                <table className='w-full border-collapse overflow-x-auto'>
+                                    <thead className='bg-gray-50 border-b-[2px]'>
+                                        <tr className=''>
+                                            <th className='text-text-medium py-[1rem] px-[1.5rem] text-left font-bold uppercase'>Commerçant</th>
+                                            <th className='text-text-medium py-[1rem] px-[1.5rem] text-left font-bold uppercase'>Plan</th>
+                                            <th className='text-text-medium py-[1rem] px-[1.5rem] text-left font-bold uppercase'>Date d'expiration</th>
+                                            <th className='text-text-medium py-[1rem] px-[1.5rem] text-left font-bold uppercase'>Statut</th>
+                                            <th className='text-text-medium py-[1rem] px-[1.5rem] text-left font-bold uppercase' >Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {abonnements.map(a => (
+                                            <tr key={a.id} className=''>
+                                                <td className='border-b-2 py-[1.25rem] px-[1.5rem]'>
+                                                    <span className='text-text-dark font-bold'>{a.commerce}</span>
+                                                </td>
+                                                <td className='border-b-2 py-[1.25rem] px-[1.5rem] '>
+                                                    <span className='text-primary-dark font-bold'>{a.plan}</span>
+                                                </td>
+                                                <td className='border-b-2 py-[1.25rem] px-[1.5rem] '>
+                                                    <span className='text-text-medium'>{a.date_expiration}</span>
+                                                </td>
+                                                <td className='border-b-2 py-[1.25rem] px-[1.5rem] '>
+                                                    <span className={`rounded-lg flex gap-2 items-center px-2 py-1 max-w-max ${a.statut === "expiré" ? "bg-red-200 text-red-700" : "bg-blue-200 text-blue-700"}`}>
+                                                        {a.statut === "expiré" ? "Expiré" : "Expire bientôt"}
+                                                    </span>
+                                                </td>
+                                                <td className='border-b-2 py-[1.25rem] px-[1.5rem] '>
+                                                    <button className='px-4 py-2 rounded-lg bg-primary-dark text-white' onClick={()=>a.statut=="expiré"?relancer():notifier()}>
+                                                        {a.statut === "expiré" ? "Relancer" : "Notifier"}
+                                                    </button>
+                                                </td>
+                                            </tr>
+                                        ))}
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+       
+    )
+}
+
+export default DashboardAdmin
