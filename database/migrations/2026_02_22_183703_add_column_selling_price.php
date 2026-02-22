b760f9156313f74b8c6332993736a6f916e93f20
MTJM <jaudelmerlando@gmail.com>
2026-02-22 20:21:05 +0100

    ajustement des migrations

---
+++ database/migrations/2026_02_22_183703_add_column_selling_price.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('produits', function (Blueprint $table) {
+            //
+            $table->decimal('selling_price', 8, 2)->nullable()->after('price');
+            $table->enum('unit', ['piece', 'kg', 'litre','carton'])->default('piece')->after('selling_price');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('produits', function (Blueprint $table) {
+            //
+        });
+    }
+};
